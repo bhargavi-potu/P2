9d36a7e36c0a87778fcf0c869e6717d7bc2c2193
lvvsndattasai <143427753+lvvsndattasai@users.noreply.github.com>
2023-09-20 20:00:27 -0400

    Added Solution

--- make-multi-filter.js
+++ make-multi-filter.js
@@ -1,1 +1,36 @@
+(function () {
+  "use strict";
 
+  function MakeMultiFilter(originalArray) {
+    var currentArray = originalArray.slice();
+    var callbacks = [];
+
+    function arrayFilterer(filterCriteria, callback) {
+      if (typeof filterCriteria === "function") {
+        currentArray = currentArray.filter(filterCriteria);
+        if (typeof callback === "function") {
+          callbacks.push(callback);
+        }
+        return arrayFilterer;
+      } else if (!filterCriteria && !callback) {
+        return currentArray;
+      } else {
+        return arrayFilterer;
+      }
+    }
+
+    arrayFilterer.applyCallbacks = function () {
+      callbacks.forEach(function (callback) {
+        callback.call(originalArray, currentArray);
+      });
+    };
+
+    return arrayFilterer;
+  }
+
+  if (typeof module !== "undefined" && typeof module.exports !== "undefined") {
+    module.exports = MakeMultiFilter;
+  } else if (typeof window !== "undefined") {
+    window.MakeMultiFilter = MakeMultiFilter;
+  }
+})();
--- template-processor.js
+++ template-processor.js
@@ -1,1 +1,15 @@
+"use strict";
 
+function TemplateProcessor(template) {
+  this.template = template;
+
+  this.fillIn = function (dictionary) {
+    return this.template.replace(/{{(.*?)}}/g, function (match, prop) {
+      return dictionary[prop] || "";
+    });
+  };
+}
+
+if (typeof module !== "undefined" && typeof module.exports !== "undefined") {
+  module.exports = TemplateProcessor;
+}
